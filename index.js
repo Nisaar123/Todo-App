0fac2e6ad224ce0d3aec015f71b1f28a3ca4c5bb
Nisar Ahmad <na623870@gmail.com>
2021-09-29 17:30:49 +0530

    changes to port for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const express = require('express') ;
-const port = process.env.port || 3000 ;
+const port = process.env.PORT || 3000 ;
 
 const db = require('./config/mongoose') ;
 const List = require('./models/list') ;
